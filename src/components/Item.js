2c29997d3c7f6a5be5feb224c11b6d7fda17b04c
mariaudovychenko <m.udovychenko@yahoo.com>
2024-04-05 01:00:01 +0200

    first commit

---
+++ src/components/Item.js
@@ -0,0 +1,18 @@
+export default function Item({ item, onDeleteItem, onToggleItem }) {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => onToggleItem(item.id)}
+      />
+      {/* // a controlled element means that el has the value defined by some state
+            and it also has an event handler which listens for the change and updates
+            the state accordingly. */}
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
+    </li>
+  );
+}
